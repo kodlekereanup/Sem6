4d612fb2ed7f6182cc482a6e80acedeb1648b6f9
kodlekereanup <anup.kodlekere@gmail.com>
2020-03-17 14:45:30 +0530

    realisation

--- AI/NQueens/Board.h
+++ AI/NQueens/Board.h
@@ -1,15 +1,18 @@
 #include <vector>
 #include <iostream>
 
+// The only job of this file is the generate the board
+
 class Board {
     public:
         int n = 8;
         std::vector<std::vector<int>> board;
 
+        // @param: nn: size of the board 
         Board(int nn);
         // Initialize N x N grid (matrix) with 0's
         void displayBoard();
-	void calCost(); //Heuristic function calculator
+	    void calCost(); //Heuristic function calculator
 };
 
 Board :: Board(int nn): n(nn) {
@@ -33,42 +36,4 @@ void Board :: displayBoard() {
     std::cout << "\n\n";
 }
 
-/*
- * TODO: 
- * Generate heuristics initially
- * Check if number of attacka are 0
- * If not, move the pieces in such a way that it reduces the heuristics
- * If a dead end is reached, randomrestart
- *
- * MORE FORMAL ALGORITHM
- *
- * Set the board to a random state
- * calculate the heuristics in the current state
- * while(values in the board is not 0) 
- * 	move the pieces in such a way that it reduces the values
- * 	bring it closer to 0 in each step
- *
- * 	if at anu point a dead end is reached (define dead end)
- * 	then NQRandomRestart 
- *	
- * Print all solutions as we keep going, keep a flag to ensure no solution is repeated twice
- *
- * HEURISTIC FUNCTION:
- * 	At any position, check the number of attacking queens using
- * 	[ N(N-1) ] / 2
- *
- * 	We can classify the attacks into three:
- * 		HORIZONTAL ATTACK: When two or more queens are in the same row
- *		VERTICAL ATTACK:   When two or more queens are in the same column
- *		DIAGONAL ATTACK:
- *			LEFT DIAGONAL
- *			RIGHT DIAGONAL
- *
- * Calculate this value at every state, filling every square
- * Main aim is to reduce all these ints to zeros.
- *
- * For any N, the number of solutions is given by:
- * 	this seems a bit difficult than I thought
- *
- *
- */
+
